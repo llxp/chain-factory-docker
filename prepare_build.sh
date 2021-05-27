f7b7b89c8ded90a35965169add043161ca9e24bf
llxp <lukasluedke@web.de>
2021-05-27 23:49:25 +0000

    made all prepare_build.sh and cleanup_build.sh scripts executable

--- prepare_build.sh
+++ prepare_build.sh
@@ -3,6 +3,6 @@
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $current_dir/services/backend_api/prepare_build.sh
-$current_dir/services/frontend_ui/prepare_build.sh
-$current_dir/services/worker_node/prepare_build.sh
+#$current_dir/services/frontend_ui/prepare_build.sh
+#$current_dir/services/worker_node/prepare_build.sh
 $current_dir/services/authentication_api/prepare_build.sh
